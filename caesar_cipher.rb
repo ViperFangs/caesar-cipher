83f3667d579fbd4a41bad6069f88a581a691565e
Aarya <amathrej@asu.edu>
2022-06-30 14:04:17 -0700

    Refactor class CaesarCipher
    Add private methods
    Change #caesar_cipher to #encrypt
    Change #caesar_cipher_helper to #encrypt_helper

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -2,6 +2,16 @@
 
 # class CaesarCipher
 class CaesarCipher
+  def encrypt(string, key)
+    # return same string if key is equal to or less than 0
+    return string if key <= 0
+
+    # split the string into an array, then call the map function to each element and finally join it back into a string
+    string.split('').map { |character| encrypt_helper(character, key) }.join('')
+  end
+
+  private
+
   DOWNCASE_LOWER_RANGE = 97
   DOWNCASE_UPPER_RANGE = 122
   UPCASE_LOWER_RANGE = 65
@@ -14,23 +24,13 @@ class CaesarCipher
     value.between?(DOWNCASE_LOWER_RANGE, DOWNCASE_UPPER_RANGE) or value.between?(UPCASE_LOWER_RANGE, UPCASE_UPPER_RANGE)
   end
 
-  def caesar_cipher_helper(character, key)
+  def encrypt_helper(character, key)
     # Check if the character is between lower case and upper case
     return character unless alphabet_range?(character)
 
     # if the current character + key is greater than Z then it shifts it back to the base value
     alphabet_range?(character.ord + key) ? (character.ord + key).chr : (character.ord - SHIFT_TO_BASE_VALUE + key).chr
   end
-
-  def caesar_cipher(string, key)
-    # return same string if key is equal to or less than 0
-    return string if key <= 0
-
-    # split the string into an array, then call the map function to each element and finally join it back into a string
-    encrypted_string = string.split('').map { |character| caesar_cipher_helper(character, key) }.join('')
-    puts encrypted_string
-    encrypted_string
-  end
 end
 
-caesar_cipher('What a string!', 5)
+puts CaesarCipher.new.encrypt('What a string!', 5)
