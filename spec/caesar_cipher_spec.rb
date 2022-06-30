1bc192fc84739016ae396d6edb04d37191fa5486
Aarya <amathrej@asu.edu>
2022-06-30 14:17:07 -0700

    Write test for encrypt

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,1 +1,10 @@
-require_relative "/caesar_cipher"
+require_relative '../caesar_cipher'
+
+describe CaesarCipher do
+  describe '#encrypt' do
+    it 'Encrypts a string' do
+      encrypted = CaesarCipher.new.encrypt('What a string!', 5)
+      expect(encrypted).to eql 'Bmfy f xywnsl!'
+    end
+  end
+end
