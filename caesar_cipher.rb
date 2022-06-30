73764943054d22101e5bcac9e1d5fae40fd08525
Aarya <amathrej@asu.edu>
2022-06-30 14:01:10 -0700

    Include all methods in Caesar Cipher Class

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,35 +1,36 @@
 # frozen_string_literal: true
 
-DOWNCASE_LOWER_RANGE = 97
-DOWNCASE_UPPER_RANGE = 122
-UPCASE_LOWER_RANGE = 65
-UPCASE_UPPER_RANGE = 90
-SHIFT_TO_BASE_VALUE = 26
+# class CaesarCipher
+class CaesarCipher
+  DOWNCASE_LOWER_RANGE = 97
+  DOWNCASE_UPPER_RANGE = 122
+  UPCASE_LOWER_RANGE = 65
+  UPCASE_UPPER_RANGE = 90
+  SHIFT_TO_BASE_VALUE = 26
 
-# predicate method to check if a character is an alphabet
-def alphabet_range?(value)
-  value = value.ord unless value.is_a? Integer
-  value.between?(DOWNCASE_LOWER_RANGE, DOWNCASE_UPPER_RANGE) or value.between?(UPCASE_LOWER_RANGE, UPCASE_UPPER_RANGE)
-end
-
-def caesar_cipher_helper(character, key)
-  # Check if the character is between lower case and upper case
-  if alphabet_range?(character)
-    alphabet_range?(character.ord + key) ? (character.ord + key).chr : (character.ord - SHIFT_TO_BASE_VALUE + key).chr
-  # return character if it isnt an alphabet
-  else
-    character
+  # predicate method to check if a character is an alphabet
+  def alphabet_range?(value)
+    value = value.ord unless value.is_a? Integer
+    value.between?(DOWNCASE_LOWER_RANGE, DOWNCASE_UPPER_RANGE) or value.between?(UPCASE_LOWER_RANGE, UPCASE_UPPER_RANGE)
   end
-end
 
-def caesar_cipher(string, key)
-  # return same string if key is 0 or less than 0
-  return string if key <= 0
+  def caesar_cipher_helper(character, key)
+    # Check if the character is between lower case and upper case
+    return character unless alphabet_range?(character)
 
-  # split the string into an array, then call the map function to each element and finally join it back into a string
-  encrypted_string = string.split('').map { |character| caesar_cipher_helper(character, key) }.join('')
-  puts encrypted_string
-  encrypted_string
+    # if the current character + key is greater than Z then it shifts it back to the base value
+    alphabet_range?(character.ord + key) ? (character.ord + key).chr : (character.ord - SHIFT_TO_BASE_VALUE + key).chr
+  end
+
+  def caesar_cipher(string, key)
+    # return same string if key is equal to or less than 0
+    return string if key <= 0
+
+    # split the string into an array, then call the map function to each element and finally join it back into a string
+    encrypted_string = string.split('').map { |character| caesar_cipher_helper(character, key) }.join('')
+    puts encrypted_string
+    encrypted_string
+  end
 end
 
 caesar_cipher('What a string!', 5)
